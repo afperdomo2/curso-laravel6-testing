9b05d880ae40252522e10bb33169ef7a0b2a064b
Andrés Perdomo <alvarado0925@gmail.com>
2022-05-26 16:29:58 -0500

    🧪Feat: Create test from store posts

---
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_store()
+    {
+        //$this->withoutExceptionHandling();
+
+        $response = $this->json('POST', '/api/posts', [
+            'title' => 'Post de prueba'
+        ]);
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => 'Post de prueba'])
+            ->assertStatus(201); // 201 = Created
+
+        $this->assertDatabaseHas('posts', ['title' => 'Post de prueba']);
+    }
+
+    public function test_validate_title()
+    {
+        $response = $this->json('POST', '/api/posts', [
+            'title' => ''
+        ]);
+
+        // 422 = Unprocessable entity
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors('title');
+    }
+}
