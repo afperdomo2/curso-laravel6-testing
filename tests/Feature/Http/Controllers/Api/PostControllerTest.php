f0df82cad931c5f585ccf3a387a3b1c4f88d77b1
Andrés Perdomo <alvarado0925@gmail.com>
2022-05-26 17:23:47 -0500

    🧪Feat: Create test from show post

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers\Api;
 
+use App\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -34,5 +35,16 @@ class PostControllerTest extends TestCase
         // 422 = Unprocessable entity
         $response->assertStatus(422)
             ->assertJsonValidationErrors('title');
+    }
+
+    public function test_show()
+    {
+        $post = factory(Post::class)->create();
+
+        $response = $this->json('GET', "/api/posts/$post->id");
+
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+            ->assertJson(['title' => $post->title])
+            ->assertStatus(200); // 200 = Accepted
     }
 }
